bfbae376ce68ce4dd8744b2b7b1bb317f2e50f96
caitlynod <caitlynodriscoll@gmail.com>
2015-11-19 20:31:57 -0500

    Project Starting Script files

---
+++ Scripts/characterScript.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+import System.Collections.Generic;//need to import Dictionary
+public var health:int = 100;
+var inventory = new Dictionary.<String, int>();
+inventory['key'] = 0;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function reduceHealth(damage : int){
+
+	health -= damage;
+	Debug.Log(health);
+}
+
