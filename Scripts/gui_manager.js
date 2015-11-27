415e7006408e92b0c3f566d7a9f662c852cf2aa3
FisherTanner <lmha8@hotmail.com>
2015-11-26 21:46:45 -0500

    Pages linked to GUI, And Font for GUI
    The Pages can be picked up and added to the inventory displaying how
    many pages are currently found when player walks through the box
    collider. Trying to add clickable pick up functionality. The font Used
    in the GUI is Chiller.

---
+++ Scripts/gui_manager.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var pagesCount:UI.Text;
+
+function Update() {
+	var player = GameObject.FindGameObjectWithTag("Main").GetComponent(inventoryScript);
+	
+	var pagesValue:int = player.page;
+	
+	if (pagesValue < 2 && pagesValue > 0) {
+		pagesCount.text = pagesValue + " " + "Page Found";
+	} else {
+		pagesCount.text = pagesValue + " " + "Pages Found";
+	}
+}
---
+++ Scripts/inventoryScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+public var page:int = 0;
+
+var inventory = new Dictionary.<String,int>();
+inventory["page"] = 1;
+
+function Start() {
+}
+
+function Update() {
+}
+
+function OnTriggerEnter(other:Collider) {
+	if(other.tag =="page") {
+	page++;
+	Destroy(other.gameObject);
+	
+	}
+}
+
+function OnMouseUpAsButton() {
+	page++;
+	Destroy(gameObject);
+}
